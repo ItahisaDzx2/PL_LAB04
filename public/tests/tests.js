dfbe0a430f920c613361d175943c3a8178e40a41
mdbGalvan <magomenlopark@gmail.com>
2014-03-07 23:19:50 +0000

    Añadiendo un tests para bexec

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,12 +1,15 @@
 var assert = chai.assert;
 
-suite('PRUEBAS PARA LA SALIDA', function() {
-	test('Soporta localStorage', function() {
-		INPUT.value = 'var a = 3;';
-        main();
-		assert.equal(OUTPUT.innerHTML,'{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": 3,\n        "arity": "literal"\n    }\n}');\n	});
+suite('PRUEBAS PARA BEXEC', function() {
+	setup(function(){
+		var str = "dBdXXXXDBBD";
+		var re = /d(b+)(d)/ig;
+	});
+	test('NULL', function() {
+		regexp.lastIndex = 3;
+		assert.equal(re.bexec(str), null);
+    });	
 });
-
 
 suite('PRUEBAS PARA COMPROBAR ERRORES', function() {	
 	test('Operador', function() {
@@ -20,4 +23,12 @@ suite('PRUEBAS PARA COMPROBAR ERRORES', function() {
         main();
 		assert.match(OUTPUT.innerHTML, /Syntax error/);
 	});
+});
+
+suite('PRUEBAS PARA LA SALIDA', function() {
+	test('Soporta localStorage', function() {
+		INPUT.value = 'var a = 3;';
+        main();
+		assert.equal(OUTPUT.innerHTML,'{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": 3,\n        "arity": "literal"\n    }\n}');
+    });	
 });
