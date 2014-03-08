cc7149622a5fb0edf6fc6f13cbeb95ec204fe307
mdbGalvan <magomenlopark@gmail.com>
2014-03-08 00:25:57 +0000

    Añadiendo prueba sobre make_parse

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -17,12 +17,6 @@ suite('PRUEBAS PARA BEXEC', function() {
 		re.lastIndex = 3;
 		assert.equal(re.bexec(str), null);
     });	
-	test('m', function() {
-		var str = "dBdXXXXDBBD";
-		var re = /d(b+)(d)/ig;
-		re.lastIndex = 7;
-		assert.equal(re.bexec(str), [ 'DBBD', 'BB', 'D', index: 7, input: 'dBdXXXXDBBD' ]);
-    });	
 });
 
 suite('PRUEBAS PARA DUMP GET Y AJAX', function() {
@@ -55,5 +49,24 @@ suite('PRUEBAS PARA LA SALIDA', function() {
 		INPUT.value = 'var a = 3;';
         main();
 		assert.equal(OUTPUT.innerHTML,'{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": 3,\n        "arity": "literal"\n    }\n}');
+    });	
+});
+
+suite('PRUEBAS PARA LA make_parse', function() {
+	test('Asignación', function() {
+		var parse = make_parse();
+		var source = 'var a = 3;';
+		var string, tree;
+		try {
+			tree = parse(source);
+			//string = JSON.stringify(tree, ['type', 'value', 'from', 'to'],  4);
+			string = JSON.stringify(tree, ['key', 'name', 'message',
+				 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+		} catch (e) {
+			string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+					'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+		};
+		assert.equal(string,'{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": 3,\n        "arity": "literal"\n    }\n}');
+		assert.equal(OUTPUT.innerHTML, string.replace(/&/g, '&amp;').replace(/[<]/g, '&lt;'));
     });	
 });
